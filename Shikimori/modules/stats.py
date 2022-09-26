c8eae86c852e56ef140526396511504760113b3e
Kira yoshikage <109851629+Yoshikage1@users.noreply.github.com>
2022-09-26 19:53:34 +0530

    Update stats.py

--- Shikimori/modules/stats.py
+++ Shikimori/modules/stats.py
@@ -103,7 +103,7 @@ def stats(update, context):
                         [                  
                             InlineKeyboardButton(
                                     text="REPO",
-                                    url="https://github.com/SOME-1HING/ShikimoriBot"),
+                                    url="https://github.com/Yoshikage1/ShimizuxRobot"),
                         ]
                     ]
                 ),
@@ -119,7 +119,7 @@ def stats(update, context):
                         [                  
                             InlineKeyboardButton(
                                     text="REPO",
-                                    url="https://github.com/SOME-1HING/ShikimoriBot"),
+                                    url="https://github.com/Yoshikage1/ShimizuxRobot"),
                         ]
                     ]
                 ),
@@ -142,7 +142,7 @@ def stats(update, context):
                   [                  
                        InlineKeyboardButton(
                                 text="REPO",
-                                url="https://github.com/SOME-1HING/ShikimoriBot"),
+                                url="https://github.com/Yoshikage1/ShimizuxRobot"),
                      ] 
                 ]
             ),
