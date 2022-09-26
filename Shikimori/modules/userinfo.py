96e26d24618a6be340f61b320cd62dbbb62958ad
Kira yoshikage <109851629+Yoshikage1@users.noreply.github.com>
2022-09-26 19:56:51 +0530

    Update userinfo.py

--- Shikimori/modules/userinfo.py
+++ Shikimori/modules/userinfo.py
@@ -244,10 +244,10 @@ def info(update: Update, context: CallbackContext):
     [
                         InlineKeyboardButton(
                              text="Health",
-                             url="https://t.me/Shikimori_bot_Updates/9"),
+                             url="https://t.me/kakashi_bots_updates/11"),
                        InlineKeyboardButton(
-                             text="Disasters",
-                             url="https://t.me/Shikimori_bot_Updates/6"),
+                             text="disasters",
+                             url="https://t.me/kakashi_bots_updates/14"),
                     ],
     ]
     user_id = extract_user(update.effective_message, args)
